a939473c381aec3f02c54845f035d8725777cd76
Brandon Chung <deltaharbinger@gmail.com>
2020-02-23 09:56:55 -0500

    Added client folder

---
+++ client/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+class RootWindow:
+	def __init__(self, win):
+		self.diagnose = Button(win, text="Diagnose").pack(side = TOP, pady = 10)
+
+top = Tk()
+mainwin = RootWindow(top)
+
+top.title("Dengue Diagnosis Assistant")
+top.geometry("800x600+10+10")
+top.mainloop()
+
