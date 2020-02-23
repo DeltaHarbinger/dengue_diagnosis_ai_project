61a4afa98ee4f848496a50606fb4ee93a0250a9c
Brandon Chung <deltaharbinger@gmail.com>
2020-02-23 10:16:23 -0500

    Renamed database to database_utils

--- client/main.py
+++ client/main.py
@@ -1,8 +1,13 @@
 from tkinter import *
+from . import database_utils
+
 
 class RootWindow:
 	def __init__(self, win):
 		self.diagnose = Button(win, text="Diagnose").pack(side = TOP, pady = 10)
+
+
+
 
 top = Tk()
 mainwin = RootWindow(top)
@@ -10,4 +15,3 @@ mainwin = RootWindow(top)
 top.title("Dengue Diagnosis Assistant")
 top.geometry("800x600+10+10")
 top.mainloop()
-
