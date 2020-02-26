8af58034c1347e693950c0173b1c7e8085393122
Brandon Chung <deltaharbinger@gmail.com>
2020-02-26 16:10:03 -0500

    Finished temperature input and refactor

--- client/main.py
+++ client/main.py
@@ -14,7 +14,7 @@ class RootWindow:
 		diagnosis_win = DiagnosticsWindow(diagnosis_header)
 
 		diagnosis_header.title("New Diagnosis")
-		diagnosis_header.geometry("600x350+10+10")
+		diagnosis_header.geometry("800x350+10+10")
 		
 		Grid.columnconfigure(diagnosis_header, 0, weight=1)
 		Grid.columnconfigure(diagnosis_header, 1, weight=2)
@@ -28,53 +28,89 @@ class RootWindow:
 		
 		diagnosis_header.mainloop()
 
-class TemperatureWindow:
-	temperature = None
+class TemperatureWindow(Toplevel):
 
 	def __init__(self, win):
-		self.title_text = Label(win, text = "Enter Temperature in °C").grid(row = 0, column = 0, columnspan = 2, sticky = N, pady = (win.winfo_reqwidth() / 5, win.winfo_reqwidth() / 10))
-		self.temperature_celcius_entry = Entry(win).grid(row = 1, column = 0, sticky = N + W + E, pady = win.winfo_reqheight() / 10, padx = win.winfo_reqwidth() / 5)
-		self.temperature_celcius_label = Label(win, text = "°C").grid(row = 1, column = 1, sticky = N + W + E, pady = win.winfo_reqheight() / 10, padx = win.winfo_reqwidth() / 5)
-		self.temperature_celcius_label = Label(win, text = self.get_farenheit()).grid(row = 2, column = 0, sticky = N + W + E, pady = win.winfo_reqheight() / 10, padx = win.winfo_reqwidth() / 5)
-		self.temperature_celcius_label = Label(win, text = "°F").grid(row = 2, column = 1, sticky = N + W + E, pady = win.winfo_reqheight() / 10, padx = win.winfo_reqwidth() / 5)
-		self.store_temterature_button = Button(win, text = "OK", command = self.store_celcius)
-		
+		Toplevel.__init__(self, win)
 
-	def store_celcius(self):
-		try:
-			self.temperature = self.temperature_celcius_entry.get()
-		except ValueError:
-			self.temperature = None
-
-	def get_farenheit(self):
-		try: 
-			return str(float(self.temperature * 1.8 + 32))
-		except ValueError:
-			return ""
+		self.temperature = None
+		self.title_text = Label(self, text = "Enter Temperature in °C").grid(row = 0, column = 0, columnspan = 2, sticky = N, pady = (self.winfo_reqwidth() / 5, self.winfo_reqwidth() / 10))
+		self.temperature_celcius_entry = Entry(self)
+		self.temperature_celcius_entry.grid(row = 1, column = 0, sticky = N + W + E, pady = self.winfo_reqheight() / 10, padx = self.winfo_reqwidth() / 5)
+		self.temperature_celcius_label = Label(self, text = "°C").grid(row = 1, column = 1, sticky = N + W + E, pady = self.winfo_reqheight() / 10, padx = self.winfo_reqwidth() / 5)
+		self.store_temterature_button = Button(self, text = "OK", command = self.on_ok)
 		
-class DiagnosticsWindow:
+		self.store_temterature_button.grid(row = 2, column = 0, columnspan = 2, sticky = N + W + E, pady = self.winfo_reqheight() / 10, padx = self.winfo_reqwidth() / 5)
+		
+		# self.temperature_celcius_entry.bind("<Button-1>", self.on_ok)
+		# self.temperature_celcius_entry.bind("<Return>", self.on_ok)
+	
+
+
+	def show(self):
+		self.wm_deiconify()
+		self.temperature_celcius_entry.focus_force()
+		self.wait_window()
+		return self.temperature
+
+	def on_ok(self, event=None):
+		self.temperature = str(self.temperature_celcius_entry.get())
+		self.destroy()
+		
+class DiagnosticsWindow(Frame):
 	name = None
 	diagnostics = {"symptoms": None, "temperature": None, "countries_visited": None}
 
 	def __init__(self, win):
+		Frame.__init__(self, win)
 
 		self.name_text = Label(win, text = "Name").grid(row = 1, column = 0, columnspan = 2, sticky = N+E+W, padx = win.winfo_reqwidth() / 5)
 		self.name_input = Entry(win).grid(row = 1, column = 2, columnspan = 2, sticky = N+E, padx = win.winfo_reqwidth() / 5)
 		
-		self.symptoms_prefix = Label(win, text = self.generate_prefix("symptoms")).grid(row = 2, column = 0, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.symptoms_text = Label(win, text = "Symptoms").grid(row = 2, column = 1, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.symptoms_status = Label(win, text = self.generate_status("symptoms")).grid(row = 2, column = 2, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.symptoms_edit = Button(win, text = "Edit").grid(row = 2, column = 3, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		self.build_symptoms_row(win)
 
-		self.temperature_prefix = Label(win, text = self.generate_prefix("temperature")).grid(row = 3, column = 0, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.temperature_text = Label(win, text = "Temperature").grid(row = 3, column = 1, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.temperature_status = Label(win, text = self.generate_status("temperature")).grid(row = 3, column = 2, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.temperature_edit = Button(win, text = "Edit").grid(row = 3, column = 3, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		self.build_temperature_row(win)
 
-		self.countries_visited_prefix = Label(win, text = self.generate_prefix("countries_visited")).grid(row = 4, column = 0, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.countries_visited_text = Label(win, text = "Cuntries Visited").grid(row = 4, column = 1, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.countries_visited_status = Label(win, text = self.generate_status("countries_visited")).grid(row = 4, column = 2, sticky = N+W, padx = win.winfo_reqwidth() / 5)
-		self.countries_visited_edit = Button(win, text = "Edit").grid(row = 4, column = 3, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		self.build_countries_visited_row(win)
+		
+	def build_symptoms_row(self, win):
+		self.symptoms_prefix = Label(win, text = self.generate_prefix("symptoms"))
+		self.symptoms_prefix.grid(row = 2, column = 0, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.symptoms_text = Label(win, text = "Symptoms")
+		self.symptoms_text.grid(row = 2, column = 1, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.symptoms_status = Label(win, text = self.generate_status("symptoms"))
+		self.symptoms_status.grid(row = 2, column = 2, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.symptoms_edit = Button(win, text = "Edit")
+		self.symptoms_edit.grid(row = 2, column = 3, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+
+	def build_temperature_row(self, win):
+		self.temperature_prefix = Label(win, text = self.generate_prefix("temperature"))
+		self.temperature_prefix.grid(row = 3, column = 0, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.temperature_text = Label(win, text = "Temperature")
+		self.temperature_text.grid(row = 3, column = 1, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.temperature_status = Label(win, text = self.generate_status("temperature"))
+		self.temperature_status.grid(row = 3, column = 2, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.temperature_edit = Button(win, text = "Edit", command = self.get_temperature)
+		self.temperature_edit.grid(row = 3, column = 3, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+
+	def build_countries_visited_row(self, win):
+		self.countries_visited_prefix = Label(win, text = self.generate_prefix("countries_visited"))
+		self.countries_visited_prefix.grid(row = 4, column = 0, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.countries_visited_text = Label(win, text = "Cuntries Visited")
+		self.countries_visited_text.grid(row = 4, column = 1, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.countries_visited_status = Label(win, text = self.generate_status("countries_visited"))
+		self.countries_visited_status.grid(row = 4, column = 2, sticky = N+W, padx = win.winfo_reqwidth() / 5)
+		
+		self.countries_visited_edit = Button(win, text = "Edit")
+		self.countries_visited_edit.grid(row = 4, column = 3, sticky = N+W, padx = win.winfo_reqwidth() / 5)
 
 	def generate_prefix(self, diagnostic):
 		if self.diagnostics[diagnostic] == None:
@@ -83,7 +119,7 @@ class DiagnosticsWindow:
 			prefix = self.diagnostics[diagnostic]
 			if diagnostic == "temperature":
 				farenheit = float(self.diagnostics[diagnostic]) * 1.8 + 32
-				return self.diagnostics[diagnostic] + " °C | " + str(farenheit) + " °F"
+				return self.diagnostics[diagnostic] + " °C | " + ("%.2f" % farenheit ) + " °F"
 			else:
 				return str(len(self.diagnostics[diagnostic])) + " Selected"
 			
@@ -92,6 +128,15 @@ class DiagnosticsWindow:
 			return "X"
 		else:
 			return "✓"
+
+	def get_temperature(self):
+		self.diagnostics["temperature"] = TemperatureWindow(self).show()
+
+		self.temperature_prefix.config(text = self.generate_prefix("temperature"))
+		self.temperature_status.config(text = self.generate_status("temperature"))
+	
+	
+
 
 root_header = Tk()
 root_win = RootWindow(root_header)
