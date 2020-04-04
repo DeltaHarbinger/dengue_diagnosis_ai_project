9c012853d28055adb236212ab2d7a0b8da26a174
Brandon Chung <deltaharbinger@gmail.com>
2020-04-03 20:59:09 -0500

    Added probability to symptoms

--- server/routes/symptoms.js
+++ server/routes/symptoms.js
@@ -3,12 +3,58 @@ const router = express.Router()
 
 const db = require('../config/database')
 const Symptom = require('../models/symptom')
+const Diagnosis = require('../models/diagnosis')
+const DiagnosisSymptom = require('../models/diagnosis_symptom')
 
 router.get('/', (req, res) => {
 	Symptom.findAll()
 		.then(symptoms => res.send({symptoms}))
 		.catch(console.log)
 })
+
+router.get('/probability', async (req, res) => {
+	let {symptomIds} = req.body
+	let totalLikelihood = 0.0
+	let currentLikelihood = 0.0
+	let symptoms = await Symptom.findAll()
+
+	if(!Array.isArray(symptomIds)) {
+		if(symptomIds === undefined){
+			symptomIds = []
+		} else {
+			symptomIds = [symptomIds]
+		}
+	}
+
+	symptomIds = symptomIds.map(id => parseInt(id))
+	
+	await Promise.all(symptoms.map(async symptom => {
+		let symptomId = symptom.id
+		let diagnosisSymptoms = await DiagnosisSymptom.findAll({where: {symptomId}})
+		let total = diagnosisSymptoms.length
+		let positive = 0
+		await Promise.all(diagnosisSymptoms.map(async ds => {
+			let diagnosis = await Diagnosis.findOne({where: {id: ds.diagnosisId}})
+			if(diagnosis.result > 50) {
+				positive += 1
+			}
+		}))
+		if(total == 0) {
+			return
+		}
+		totalLikelihood += positive / parseFloat(total)
+		if(symptomIds.includes(symptomId)){
+			currentLikelihood += positive / parseFloat(total)
+		}
+	}))
+
+	if(totalLikelihood == 0){
+		res.send({probability: 0})
+	} else {
+		res.send({probability: currentLikelihood / parseFloat(totalLikelihood)})
+	}
+})
+
 
 router.post('/', (req, res) => {
 	let {name} = req.body
